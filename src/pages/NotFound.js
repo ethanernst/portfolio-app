1866a2b43bbc07a3dc0379aeb24abd295ce8a893
ethanernst <ethanernst1@gmail.com>
2023-05-13 20:41:01 -0400

    added scripts to allow an SPA on gh pages

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,6 +1,30 @@
 import PageLayout from '../components/Layout/PageLayout';
 
 function NotFound() {
+  // script to enable SPA for github pages from https://github.com/rafgraph/spa-github-pages
+  var pathSegmentsToKeep = 1;
+
+  var l = window.location;
+  l.replace(
+    l.protocol +
+      '//' +
+      l.hostname +
+      (l.port ? ':' + l.port : '') +
+      l.pathname
+        .split('/')
+        .slice(0, 1 + pathSegmentsToKeep)
+        .join('/') +
+      '/?/' +
+      l.pathname
+        .slice(1)
+        .split('/')
+        .slice(pathSegmentsToKeep)
+        .join('/')
+        .replace(/&/g, '~and~') +
+      (l.search ? '&' + l.search.slice(1).replace(/&/g, '~and~') : '') +
+      l.hash
+  );
+
   return (
     <PageLayout>
       <h1 className="title">Page not found</h1>
